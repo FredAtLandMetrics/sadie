20633691097f37ad22a2f7205268331409bddd7c
Fred McDavid <fred@landmetrics.com>
2012-04-16 23:41:38 -0400

    olsen prep

--- lib/sadie/primer_plugins/IniFile.plugin.rb
+++ lib/sadie/primer_plugins/IniFile.plugin.rb
@@ -2,17 +2,17 @@ Sadie::registerPrimerPlugin( {  "match" => /\.ini$/,
                                 "accepts-block" => false,
                                 "prime-on-init" => true } ) do |sadie, key_prefix, primer_file_filepath|
     
-    puts "processing ini file: primer_file_filepath"
+    puts "processing ini file: #{primer_file_filepath}"
     
     ini_file_basename = File.basename primer_file_filepath
     ini_file_root = ini_file_basename.gsub( /\.ini$/, "" )
     
     if inihash = Sadie::iniFileToHash( primer_file_filepath )
-        
+        puts "got hash"
         inihash.each do |section, section_hash|
-            
+            puts "got at least one section"
             section_hash.each do |key, value|
-                
+                puts "got at least one key"
                 key_to_set =  key_prefix + "." + ini_file_root + "." + section + "." + key
                 key_to_set = key_to_set.gsub(/^\./,"")
                 sadie.set( key_to_set, value )
