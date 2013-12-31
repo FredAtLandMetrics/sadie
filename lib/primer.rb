5668ba380b6232e938683637e32551481db3f496
landmetrics <fred@landmetrics.com>
2013-12-31 15:01:17 -0500

    added code and tests to handle keys as strings or arrays of strings

--- lib/primer.rb
+++ lib/primer.rb
@@ -1,6 +1,6 @@
 class Primer
   
-  attr_accessor :keys, :mode, :storage_manager, :storage_mechanism, :current_keys
+  attr_accessor :keys, :mode, :storage_manager, :storage_mechanism, :assign_keys
   
   def initialize( params=nil )
     self.storage_mechanism = :memory
@@ -21,22 +21,67 @@ class Primer
     end
   end
   
-  def prime( k )
-    self.keys = k.is_a?( Array ) ? k : [k]
+  def _validate_key_arg( k=nil )
+    if k.is_a?(String)
+      true
+    elsif k.is_a?( Array )
+      k.each do |key|
+        unless key.is_a?( String )
+          raise 'keys must be string or array or strings'
+        end
+      end
+    else
+      raise 'keys must be string or array or strings'
+    end
+    true
+  end
+  
+  def prime( k=nil )
+    self.keys = Array(k) if _validate_key_arg(k)
     yield if block_given?
   end
   
   def assign( keys = nil )
-    self.current_keys = keys
+    if keys.nil?
+      self.assign_keys = self.keys
+    elsif _validate_key_arg(keys)
+      unless (Array( keys ) - self.keys).empty?
+        raise 'assigning keys that are not given as arguments to prime is not permitted'
+      end
+      self.assign_keys = Array(keys)
+    end
     unless mode == :registration
       yield if block_given?
     end
   end
   
-  def set( value )
+  def set( keys=nil, value=nil)
+    if value.nil?
+      _set1(keys)
+    else
+      _set2(keys,value)
+    end
+  end
+    
+  def _set2( keys=nil, value )
+    if keys.nil?
+      _set1(value)
+    elsif _validate_key_arg(keys)
+      unless (Array( keys ) - self.assign_keys).empty?
+        raise 'assigning keys that are not given as arguments to assign (or to prime, if assign was given no keys as arguments) is not permitted'
+      end
+      self.storage_manager.set(
+        :mechanism => self.storage_mechanism,
+        :keys => Array(keys),
+        :value => value
+      )
+    end
+  end
+  
+  def _set1( value )
     self.storage_manager.set(
       :mechanism => self.storage_mechanism,
-      :keys => self.current_keys.nil? ? self.keys : self.current_keys,
+      :keys => self.assign_keys,
       :value => value
     )
   end
--- spec/primer.rb
+++ spec/primer.rb
@@ -18,8 +18,10 @@ describe Primer do
     mech = SadieStorageMechanismMemory.new
     storage.register_storage_mechanism :memory, mech
     p = Primer.new( :storage_manager => storage )
-    p.assign [ "simple.test" ] do
-      p.set( "simple.value" )
+    p.prime [ "simple.test"] do
+      p.assign [ "simple.test" ] do
+        p.set( "simple.value" )
+      end
     end
     mech.get( "simple.test" ).should == "simple.value"
   end
@@ -33,4 +35,71 @@ describe Primer do
     mech.get( "minimal.primer" ).should == "testval"
   end
   
+  it "should not allow assignment for keys not mentioned in the prime directive" do
+    expect {
+      storage = SadieStorageManager.new
+      mech = SadieStorageMechanismMemory.new
+      storage.register_storage_mechanism :memory, mech
+      p = Primer.new( :storage_manager => storage )
+      p.prime [ "simple.test"] do
+        p.assign [ "simple.other" ]
+      end
+    }.to raise_error
+    
+  end
+  
+  it "should not allow set for keys not mentioned in the assign directive" do
+    expect {
+      storage = SadieStorageManager.new
+      mech = SadieStorageMechanismMemory.new
+      storage.register_storage_mechanism :memory, mech
+      p = Primer.new( :storage_manager => storage )
+      p.prime [ "simple.test"] do
+        p.assign [ "simple.test" ] do
+          p.set ["simple.other"], "someval"
+        end
+      end
+    }.to raise_error
+  end
+  
+  it "should be ok to use strings instead of arrays for prime" do
+    storage = SadieStorageManager.new
+    mech = SadieStorageMechanismMemory.new
+    storage.register_storage_mechanism :memory, mech
+    p = Primer.new( :storage_manager => storage )
+    p.prime "simple.test" do
+      p.assign [ "simple.test" ] do
+        p.set "someval"
+      end
+    end
+    mech.get("simple.test").should == "someval"
+  end
+  
+  it "should be ok to use strings instead of arrays for assign" do
+    storage = SadieStorageManager.new
+    mech = SadieStorageMechanismMemory.new
+    storage.register_storage_mechanism :memory, mech
+    p = Primer.new( :storage_manager => storage )
+    p.prime ["simple.test"] do
+      p.assign "simple.test" do
+        p.set "someval"
+      end
+    end
+    mech.get("simple.test").should == "someval"
+  end
+  
+  it "should be ok to use strings instead of arrays for set" do
+    storage = SadieStorageManager.new
+    mech = SadieStorageMechanismMemory.new
+    storage.register_storage_mechanism :memory, mech
+    p = Primer.new( :storage_manager => storage )
+    p.prime ["simple.test"] do
+      p.assign "simple.test" do
+        p.set "simple.test","someval"
+      end
+    end
+    mech.get("simple.test").should == "someval"
+  end
+  
+
 end
