90b3bf33f76776a0b331748914734c8b00099b1f
landmetrics <fred@landmetrics.com>
2014-01-09 13:57:21 -0500

    removing debugging statements

--- test/v2/test_installation/primers/primer_wait_secondary.rb
+++ test/v2/test_installation/primers/primer_wait_secondary.rb
@@ -1,18 +1,13 @@
 prime ["wait.secondary"] do
   refresh 1
   before :each do
-#     puts "before pre"
     $counter ||= 0
     $max ||= 0
     $counter += 1
     $max = $counter if $counter > $max
-    puts "counter: #{$counter}, max: #{$max}"
   end
   after :each do
-#     puts "after pre"
     $counter -= 1
-#     puts "after post"
-    puts "counter: #{$counter}, max: #{$max}"
   end
   assign do
     sleep 1
