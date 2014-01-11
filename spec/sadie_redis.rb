e8c704e2a0396b7e67fbdd7979bfb7443739fac6
landmetrics <fred@landmetrics.com>
2014-01-11 01:08:57 -0500

    added tests confirming functionality of the redis-based storage mechanism

--- spec/sadie_redis.rb
+++ spec/sadie_redis.rb
@@ -1,23 +1,21 @@
 $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
 $:.unshift File.join(File.dirname(__FILE__), '..')
 
+require 'rack/test'
+require 'sadie_server'
+require 'sadie_session'
+require 'pp'
 
-# require 'bin/sadie_server'
+class SadieServer
+  def self.proc_args( argv=nil )
+    { :framework_dirpath => File.join( File.dirname( __FILE__ ), '..','test','v2','redis_test_installation' ) }
+  end
+end
+
+require 'bin/sadie_server'
+  
 
 describe 'RedisBasedSadie' do
-  
-  require 'rack/test'
-  require 'sadie_server'
-  require 'sadie_session'
-  require 'pp'
-
-  class SadieServer
-    def self.proc_args( argv=nil )
-      { :framework_dirpath => File.join( File.dirname( __FILE__ ), '..','test','v2','redis_test_installation' ) }
-    end
-  end
-  
-  require 'bin/sadie_server'
   
   describe SadieServer do
     include Rack::Test::Methods
@@ -52,16 +50,12 @@ describe 'RedisBasedSadie' do
       
   end
   
-  
   describe SadieSession do
     
     before :each do
       system 'install -d /tmp/sadie-test-keystor'
       @server = SadieServer.new( SadieServer::proc_args )
-      def @server.get_session
-        @sadie_session
-      end
-      @session = @server.get_session
+      @session = @server.instance_variable_get(:@sadie_session)
     end
     
     after :each do
@@ -69,11 +63,65 @@ describe 'RedisBasedSadie' do
     end
     
     it "should register a redis based storage mechanism when redis params exist" do
-      def @session.get_storage_manager
-        @storage_manager
-      end
-      @session.get_storage_manager.mechanism_is_registered?( :redis ).should be_true
+      storage_mgr = @session.instance_variable_get(:@storage_manager)
+      storage_mgr.mechanism_is_registered?( :redis ).should be_true
     end
+    
+  end
+  
+  describe SadieStorageMechanismRedis do
+    before :each do
+      system 'install -d /tmp/sadie-test-keystor'
+      @server = SadieServer.new( SadieServer::proc_args )
+      @session = @server.instance_variable_get(:@sadie_session)
+      @storage_mgr = @session.instance_variable_get(:@storage_manager)
+      @redis_storage_mechanism = @storage_mgr.instance_variable_get(:@mechanisms)[:redis]
+      @redis_server = @redis_storage_mechanism.instance_variable_get(:@redis_server)
+      @redis_server.flushall
+    end
+    
+    after :each do
+      system 'rm -rf /tmp/sadie-test-keystor'
+    end
+    
+    it "should be able to get and set" do
+      @redis_storage_mechanism.set("testkey","testval")
+      @redis_storage_mechanism.get("testkey").should == "testval"
+    end
+    
+    it "should have a functional has_key? method" do
+      @redis_storage_mechanism.has_key?( "somekey.test" ).should be_false
+      @redis_storage_mechanism.set 'somekey.test','some_value'
+      @redis_storage_mechanism.has_key?( "somekey.test" ).should be_true
+    end
+    
+    it "should have a functional unset method" do
+      @redis_storage_mechanism.set 'somekey.test','some_value'
+      @redis_storage_mechanism.has_key?( "somekey.test" ).should be_true
+      @redis_storage_mechanism.unset 'somekey.test'
+      @redis_storage_mechanism.has_key?( "somekey.test" ).should be_false
+    end
+    
+    
+    it "should return the same metadata that was given to it" do
+      metadata_to_give = {
+        :type => :string,
+        :awesomeness_level => :excrutiatingly
+      }
+      metadata_to_test = {
+        :type => :string,
+        :awesomeness_level => :excrutiatingly
+      }
+      wrong_metadata_to_test = {
+        :type => :integer,
+        :awesomeness_level => :excrutiatingly
+      }
+      @redis_storage_mechanism.set 'somekey.test','some_value', :metadata => metadata_to_give
+      fetched_meta = @redis_storage_mechanism.metadata( 'somekey.test' )
+      ( fetched_meta == metadata_to_test ).should be_true
+      ( fetched_meta == wrong_metadata_to_test ).should be_false
+    end    
+    
     
   end
   
